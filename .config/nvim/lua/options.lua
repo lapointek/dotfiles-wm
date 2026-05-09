6872ab3ff27d71f856c63e8b39f071edac0a85f8
lapointek <lapointek.dev@gmail.com>
2026-05-09 13:40:39 -0400

    chore: update waybar - Readd updates module chore(options): update nvim config

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -6,12 +6,25 @@ require "nvchad.options"
 -- o.cursorlineopt ='both' -- to enable cursorline!
 
 -- File Handling Options
-vim.opt.autoread = true                                 -- Write the contents, if it has been modified
-vim.opt.autowrite = true                                -- Write the contents before a command
-vim.opt.backup = false                                  -- Make a backup before overwriting a file
-vim.opt.writebackup = false                             -- Make a backup before overwriting a file
-vim.opt.swapfile = false                                -- Whether to use swap files
-vim.opt.undofile = true                                 -- Enable persistent undo
-vim.opt.undodir = vim.fn.expand("~/.vim/nvim-undo")     -- Directory to store undo file
+-- Write the contents, if it has been modified
+vim.opt.autoread = true
+-- Write the contents before a command
+vim.opt.autowrite = true
+-- Make a backup before overwriting a file
+vim.opt.backup = false
+-- Make a backup before overwriting a file
+vim.opt.writebackup = false
+-- Whether to use swap files
+vim.opt.swapfile = false
+-- Enable persistent undo
+vim.opt.undofile = true
+-- Directory to store undo file
+vim.opt.undodir = vim.fn.expand("~/.vim/nvim-undo")
 vim.opt.undolevels = 10000
 vim.opt.undoreload = 10000
+-- Minimum numver of screen lines to keep above cursor
+vim.opt.scrolloff = 10
+-- Relative line numbers
+vim.opt.relativenumber = true
+-- Wrap text
+vim.opt.wrap = false
